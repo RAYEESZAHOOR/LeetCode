475351bcc1eccf2eba5b10a8e3cb00e6fda545e0
Rayees Zahoor <rayeesxahoor@gmail.com>
2024-08-31 10:36:47 +0530

    leetcode daily

---
+++ Daily/max-prob.py
@@ -0,0 +1,30 @@
+# 1514. Path with Maximum Probability
+# Solved
+# Medium
+# Topics
+# Companies
+# Hint
+# You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
+
+# Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
+
+# If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5.
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        dist = [0] * n
+        dist[start] = 1
+        
+        for _ in range(n - 1):
+            updated = False
+            for i, (u, v) in enumerate(edges):
+                if dist[u] * succProb[i] > dist[v]:
+                    dist[v] = dist[u] * succProb[i]
+                    updated = True
+                if dist[v] * succProb[i] > dist[u]:
+                    dist[u] = dist[v] * succProb[i]
+                    updated = True
+            if not updated:
+                break
+        
+        return dist[end]
