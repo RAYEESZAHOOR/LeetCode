fa8c85dc94d49209a2489e7d4404bd43e5503ae5
Rayees Zahoor <rayeesxahoor@gmail.com>
2024-09-11 10:43:45 +0530

    daily leetccode

---
+++ Daily/Minimum Bit Flips to Convert Number.py
@@ -0,0 +1,21 @@
+# # Minimum Bit Flips to Convert Number
+# #easy
+# A bit flip of a number x is choosing a bit in the binary representation of x and flipping it from either 0 to 1 or 1 to 0.
+
+# For example, for x = 7, the binary representation is 111 and we may choose any bit (including any leading zeros not shown) and flip it. We can flip the first bit from the right to get 110, flip the second bit from the right to get 101, flip the fifth bit from the right (a leading zero) to get 10111, etc.
+# Given two integers start and goal, return the minimum number of bit flips to convert start to goal.
+
+
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        
+        # XOR to find differing bits between start and goal
+        xor_result = start ^ goal
+        count = 0
+        
+        # Count the number of 1's in the XOR result
+        while xor_result > 0:
+            count += xor_result & 1  # Check if the least significant bit is 1
+            xor_result >>= 1         # Right shift to check the next bit
+        
+        return count
